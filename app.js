adc352fab9c52f383317c3f4dd5e70a1533842bd
Gaetan PELTIER <gaetan.peltier@protonmail.com>
2019-07-18 22:33:27 +0200

    Add mongodb and mongoose

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
+// Express
 const express = require('express')
 const app = express()
 port = 3000
+
+// Mongoose
+var mongoose = require('mongoose')
+const dbRoute = 'mongodb+srv://enkienki:Rmn2010%2a@cluster0-wluuq.mongodb.net/yelpCamp?retryWrites=true&w=majority'
+mongoose.connect(dbRoute, { useNewUrlParser: true })
+let db = mongoose.connection
+db.once('open', () => console.log('connected to database'))
+
 // A light-weight module that brings window.fetch to Node.js
 const fetch = require('node-fetch')
+
 // Node.js body parsing middleware
 const bodyParser = require('body-parser')
-
 app.use(bodyParser.urlencoded({ extended: true }))
+
+// used to route the app.css to public folder
 app.use(express.static('public'))
+
 // allow to not use the extension .ejs after file names
 app.set('view engine', 'ejs')
 
-var campgrounds = [
+/* var campgrounds = [
     {
         name: "Goat Mountain",
         image: "https://images.unsplash.com/photo-1504851149312-7a075b496cc7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60",
@@ -36,22 +48,39 @@ var campgrounds = [
         name: "Goat Mountain",
         image: "https://images.unsplash.com/photo-1504851149312-7a075b496cc7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60",
     },
-]
+] */
 
+// Set Campground Model
+var CampgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+})
+var Campground = mongoose.model('Campground', CampgroundSchema)
+
+// Route to homepage
 app.get('/', (req, res) => res.render('home'))
 
+// Route to Campgrounds page
 app.get('/campgrounds', (req, res) => {
-    res.render('campgrounds', { campgrounds: campgrounds })
+    //retrive existing campgrounds from DB
+    Campground.find({}, (err, campgrounds) => {
+        err ? 
+        console.log(err)
+        : 
+        //display campgrounds from DB
+        res.render('campgrounds', { campgrounds: campgrounds })   
+    })   
 })
 
 app.get('/new', (req, res) => res.render('new'))
 
+// add new campground to the DB coming from the form
 app.post('/addNewCamp', (req, res) => {
-    const newCamp = {
+    Campground.create({
         name : req.body.newCampName,
         image: req.body.newCampImage,
-    }
-    campgrounds = [...campgrounds, newCamp]
+    },
+        (err, camp) => { err ? console.log(err) : console.log(camp) })
     res.redirect('/campgrounds')
 })
 
